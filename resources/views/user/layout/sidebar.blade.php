88eb9f61a43266e363a077ef0ffba5564ef3e182
kp-makwana <makwanakiran169@gmail.com>
2022-01-21 10:08:44 +0530

    -

--- resources/views/user/layout/sidebar.blade.php
+++ resources/views/user/layout/sidebar.blade.php
@@ -30,6 +30,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="{{ asset('css/product/product.css') }}">
     {{--    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">--}}
     @stack('style')
