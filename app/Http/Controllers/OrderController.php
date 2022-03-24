d1bd96b78cff0128326092047a48e6106f50a91f
Kiran Makwana <kiranmakwana.nl@gmail.com>
2022-03-24 18:48:06 +0530

    - Order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\DeliveryAddress;
+use App\Models\Order;
+use App\Models\OrderDetails;
+use App\Models\OrderStatus;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+    public function payment()
+    {
+        return view('user.Cart.payment');
+    }
+
+    public function placeOrder()
+    {
+        $data = CartController::summary();
+
+        $address = DeliveryAddress::where('user_id', Auth::id())->where('default_address', 1)->first(['id']);
+        $order_status = OrderStatus::first();
+
+        $date = date('Y-m-d', strtotime(now()));
+
+        $order = new Order;
+        $order->user_id = Auth::id();
+        $order->order_date = $date;
+        $order->order_amount = $data['price'];
+        $order->discount = $data['discount'];
+        $order->shipping_amount = $data['delivery_Charges'];
+        $order->shipping_amount = $data['delivery_Charges'];
+        $order->shipping_date = $data['total_price'] ?? 0;
+        $order->shipping_date = $date;
+        $order->shipping_address_id = $address->id;
+        $order->billing_address_id = $address->id;
+        $order->order_status_id = $order_status->id;
+
+        if ($order->save()) {
+            foreach ($data['carts'] as $item) {
+                $order_item = new OrderDetails;
+                $order_item->order_id = $order->id;
+                $order_item->product_id = $item['id'];
+                $order_item->quantity = $item['count'];
+                $order_item->price = $item['price'];
+                $order_item->discount = $item['product_discount'];
+                $order_item->save();
+            }
+        }
+        return view('user.order');
+    }
 }
